71c73419c19b42a9bf038781e71d6f80d0e42a0e
Enrique Bercian <qbercian@hotmail.com>
2020-09-29 17:15:15 -0400

    Added a tab bar button that shows an additional screen This screen displays Superhero movies on a Grid View.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Enrique Bercian on 9/29/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ Flix/MovieGridViewController.swift
@@ -0,0 +1,79 @@
+//
+//  MovieGridViewController.swift
+//  Flix
+//
+//  Created by Enrique Bercian on 9/29/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var movies = [[String: Any]]()
+    @IBOutlet weak var collectionGridView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionGridView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterpath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseUrl + posterpath)
+        
+        cell.posterView.af_setImage(withURL: posterURL!)
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionGridView.delegate = self
+        collectionGridView.dataSource = self
+        
+        let layout = collectionGridView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2 ) / 3
+        layout.itemSize = CGSize(width: width, height:width * 3 / 2)
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+            print(error.localizedDescription)
+            
+           } else if let data = data {
+            
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            self.movies = dataDictionary["results"] as! [[String: Any]]
+            self.collectionGridView.reloadData()
+           }
+            
+        }
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
